870c0636b4fa7fbac11ced5e0a4e703732d6fb05
Andrew-Delgado <andrewdelgado017@gmail.com>
2020-03-31 20:41:56 -0700

    creating a car object and printing it

--- car.py
+++ car.py
@@ -1,4 +1,8 @@
-def __init__(self, color, model, year):
-    self.color = color
-    self.model = model
-    self.year = year
+class Car:
+    def __init__(self, color, model, year):
+        self.color = color
+        self.model = model
+        self.year = year
+
+    def __str__(self):
+        return (f"Color : {self.color} | Model: {self.model} | Year: {self.year}")
---
+++ main.py
@@ -0,0 +1,5 @@
+import car
+
+my_car = car.Car("silver", "Chevy Cobalt", 2006)
+
+print(my_car)
