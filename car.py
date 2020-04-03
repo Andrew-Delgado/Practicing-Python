20ba61ff1c02de6e09ff6a7bc4422c62c10b00ca
Andrew-Delgado <andrewdelgado017@gmail.com>
2020-04-02 19:44:28 -0700

    changed main.py to move files based of their file type

--- car.py
+++ car.py
@@ -3,6 +3,7 @@ class Car:
         self.color = color
         self.model = model
         self.year = year
+        self.__cupholders = 5
 
     def __str__(self):
         return (f"Color : {self.color} | Model: {self.model} | Year: {self.year}")
--- main.py
+++ main.py
@@ -1,5 +1,46 @@
-import car
+import os
 
-my_car = car.Car("silver", "Chevy Cobalt", 2006)
+source = 'C:\\Users\\Andrew\\Downloads'
 
-print(my_car)
+listOfFiles = os.listdir(source)
+print(f"Original number of files: {len(listOfFiles)}")
+
+for file in listOfFiles:
+    source = 'C:\\Users\\Andrew\\Downloads'
+    destination = 'C:\\Users\\Andrew\\Documents\\UnsortedFiles\\UnsortedImages'
+
+    if file.__contains__('.jpg'):
+        src = source + '\\' + file
+        destination = destination + '\\' + file
+
+        os.rename(src, destination)
+
+        print(f"Moved file: {file}")
+
+
+print(f"\nNumber of files moved: {len(listOfFiles) - len(os.listdir(source))}")
+
+
+
+
+
+
+
+
+
+
+# print(f"This is a list of: {os.listdir(source)}")
+
+# fileText = listOfFiles[3]
+
+# src = source + '\\' + fileText
+# destination = destination + '\\' + fileText
+
+
+# print(fileText)
+# print(f"source file {src}")
+# print(f"dest file {destination}")
+
+# os.rename(src, destination)
+
+# print(f"\nThis is a list of: {os.listdir(source)}")
